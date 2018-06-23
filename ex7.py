13cdec4ddbb96a7c3783c70edb4e6cf7f896a51b
Zinmarlwin711 <zinzin9546@gmail.com>
2018-06-22 21:07:27 -1200

    more printing

--- ex7.py
+++ ex7.py
@@ -1,7 +1,7 @@
 print("Mary had a littlw lamb.")
 print("Its fleece was white as {}.".format('snow'))
 print("And everywhere that Mary went.")
-print("." * 10 ) # what'd that do?
+print("." * 10)   # what'd that do?
 
 
 end1 = "C"
@@ -18,6 +18,6 @@ end11 = "e"
 end12 = "r"
 
 
-#watch end = ' ' at the end. try removing it to see what happens
-print(end1 + end2 + end3 + end4 + end5 + end6,end=' ')
+# watch end = ' ' at the end. try removing it to see what happens
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
 print(end7 + end8 + end9 + end10 + end11 + end12)
