acd6d2fbe7d967376eb21235a0258f486d1accee
Zinmarlwin711 <zinzin9546@gmail.com>
2018-06-22 21:14:06 -1200

    Readig and writing files

--- ex16.py
+++ ex16.py
@@ -1,6 +1,7 @@
 from sys import argv 
 
-script, filename = argv
+script = argv
+filename = argv
 
 print(f"We're going to erase {filename},")
 print("If you don't want that, hit CTRL-C (^C).")
@@ -9,7 +10,7 @@ print("If you do want that, hit RETURN.")
 input("?")
 
 print("Opening the file...")
-target = open(filename,'w')
+target = open(filename, 'w')
 
 print("Truncating the file. Goodbye!")
 target.truncate()
